ef0f747742fe3d24c2d359a20a3d43d20773f7ef
Meron Hailu <hailumeron03@gmail.com>
2024-10-09 08:33:32 +0300

    Add footer component with copyright and developer name

--- recipe-finder/src/component/Footer.jsx
+++ recipe-finder/src/component/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="  w-full bg-gradient-to-b from-purple-300 to-purple-200 p-3 text-center text-sm fixed bottom-0">
-      <p>&copy; Copyright 2024,made by meron hailu</p>
+      <p>&copy; Copyright 2024, made by meron hailu</p>
     </div>
   );
 };
