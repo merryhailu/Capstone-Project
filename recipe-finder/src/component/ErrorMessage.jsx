afa60f3db9ef16b890f7f9f75e4e15a5a319755b
Meron Hailu <hailumeron03@gmail.com>
2024-10-09 08:24:56 +0300

    feat:adding error message component

--- recipe-finder/src/component/ErrorMessage.jsx
+++ recipe-finder/src/component/ErrorMessage.jsx
@@ -1,5 +1,5 @@
 const ErrorMessage = ({ message }) => {
-  return <div className="text-red-500">{message}</div>;
+  return <div className="text-red-500 font-serif">{message}</div>;
 };
 
 export default ErrorMessage;
